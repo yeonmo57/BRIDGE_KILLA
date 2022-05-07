5fbd5332ecc6eb6e2f12f9a8b5b72678b6422355
happyjmkk <hepachupa@gmail.com>
2022-05-07 22:15:15 +0900

    Intro#4 개발 완료

---
+++ Assets/Scripts/Intro/IntroTest_4.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroTest_4 : MonoBehaviour
+{
+    public float cameraSpeed;
+    public Camera cam;
+
+    public GameObject canvas;
+    public Image gameStart;
+    public Image gameLoad;
+
+    Vector3 targetPosition;
+    
+    // 페이드인 시간 조절
+    float time = 0f;
+    float F_time = 2f;
+
+    void Start()
+    {
+        targetPosition = new Vector3(0, 0, -10);  
+    }
+
+
+    void Update()
+    {
+        cam.transform.position = Vector3.Lerp(cam.transform.position, targetPosition, cameraSpeed * Time.deltaTime);
+
+        StartCoroutine(B_Visible());
+    }
+
+    IEnumerator B_Visible()
+    {
+        yield return new WaitForSeconds(5.7f);
+        gameStart.gameObject.SetActive(true);
+        gameLoad.gameObject.SetActive(true);
+
+        Color alphaS = gameStart.color;
+        Color alphaL = gameLoad.color;
+
+        while (alphaS.a < 1 && alphaL.a < 1)
+        {
+            time += Time.deltaTime / F_time;
+            alphaS.a = Mathf.Lerp(0, 3, time);
+            alphaL.a = Mathf.Lerp(0, 3, time);
+            gameStart.color = alphaS;
+            gameLoad.color = alphaL;
+            yield return null;
+        }
+
+     
+    
+
+    }
+
+}
