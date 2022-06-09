10cdd73cb9e9ef1b9a2621849af10fd9df159312
ymthebest <ymthebest57@gmail.com>
2022-06-09 21:36:58 +0900

    제조 완성

--- Assets/Scripts/Make/MakeController.cs
+++ Assets/Scripts/Make/MakeController.cs
@@ -9,16 +9,23 @@ using UnityEngine.UI;
 //FieldItems 스크립트의 응용
 public class MakeController : MonoBehaviour
 {
-    //말풍선 이미지
     public GameObject log1;
     public GameObject log2;
     public GameObject log3;
 
     public GameObject EndBlackBox;
+    public GameObject EndBlackBox2;
+
+    public GameObject Making_IMG1;
+    public GameObject Making_IMG2;
+    public GameObject Making_IMG3;
+
+    public GameObject madecassol;
 
     //개수를 나타내는 텍스트
     private Text lily;
     private Text stock; //스토크라는 꽃 이름 입니다(우리말: 비단향꽃무)
+    private Text Medecine_Name;
     public int numlily;
     public int numstock;
 
@@ -28,25 +35,30 @@ public class MakeController : MonoBehaviour
     public Sprite stock_img;
     public Sprite serve_img; //제공 손바닥 
     public Sprite play_img; //화살표
+    public Sprite lily_making_img;
+    //public Sprite stock_making_img;
 
     public GameObject Buttonlily;
     public GameObject Buttonstock;
 
-    //Image thisImg; //현재 이미지
-
     private int numClick=0; //몇번째 아이템 선택인지
+
+    bool serve;//false일때는 아이템 만드는 함수, true가 되면 제공하는 기능
+
 
     private void Start()
     {
         lily = GameObject.Find("Material_Name_Lily").GetComponent<Text>();
-        stock = GameObject.Find("Material_Name_Stock").GetComponent<Text>();
+        //stock = GameObject.Find("Material_Name_Stock").GetComponent<Text>();
 
-        //thisImg = GetComponent<Image>();
+        Medecine_Name = GameObject.Find("Make_CompletionName").GetComponent<Text>();
 
         numlily = 9;
         numstock = 9;
         lily.text = numlily.ToString() + "개";
-        stock.text = numlily.ToString() + "개";
+        //stock.text = numlily.ToString() + "개";
+
+        serve = false;
     }
 
     void Update()
@@ -63,9 +75,6 @@ public class MakeController : MonoBehaviour
         
         //몇번째 선택인지 변수 증가
         numClick += 1;
-
-        //이미지 변경
-        //이미지 활성화
 
         if (numClick == 1)
         {
@@ -87,6 +96,7 @@ public class MakeController : MonoBehaviour
         choiceEnd();
     }
 
+    /*
     public void Onclickstock()
     {
         numstock -= 1;
@@ -110,23 +120,19 @@ public class MakeController : MonoBehaviour
         }
         choiceEnd();
     }
+    */
 
     //3번 눌러서 선택 끝났을때
     public void choiceEnd()
     {
         GameObject PlayButton = GameObject.Find("Play_Button");
-        //PlayButton.GetComponent<Button>().interactable = false;
 
         if (numClick == 3)
         {
             Buttonlily.GetComponent<Button>().interactable = false;
-            Buttonstock.GetComponent<Button>().interactable = false;
-
-            PlayButton.GetComponent<Image>().sprite = serve_img;
-            PlayButton.GetComponent<Button>().interactable = true;
-
-            //GameObject EndBlackBox = GameObject.Find("EndBlackBox");
+            //Buttonstock.GetComponent<Button>().interactable = false;
             
+            PlayButton.GetComponent<Button>().interactable = true;    
         }
     }
 
@@ -135,7 +141,7 @@ public class MakeController : MonoBehaviour
         //전부 초기화
         GameObject PlayButton = GameObject.Find("Play_Button");
         Buttonlily.GetComponent<Button>().interactable = true;
-        Buttonstock.GetComponent<Button>().interactable = true;
+        //Buttonstock.GetComponent<Button>().interactable = true;
 
         PlayButton.GetComponent<Image>().sprite = play_img;
         PlayButton.GetComponent<Button>().interactable = false;
@@ -144,7 +150,7 @@ public class MakeController : MonoBehaviour
         numlily = 9;
         numstock = 9;
         lily.text = numlily.ToString() + "개";
-        stock.text = numlily.ToString() + "개";
+        //stock.text = numlily.ToString() + "개";
 
         log1.SetActive(false);
         log2.SetActive(false);
@@ -155,37 +161,66 @@ public class MakeController : MonoBehaviour
 
     public void OnclickPlay()
     {
-        //블랙박스가 화면을 덮음
-        EndBlackBox.SetActive(true);
-        StartCoroutine(EndAnimate());
+        if (serve == false)
+        {
+            GameObject PlayButton = GameObject.Find("Play_Button");
+            PlayButton.GetComponent<Button>().interactable = false;
 
-
-        //마데카솔등장
-        //블랙박스 하나더 등장, 마데카솔 커짐(?), 콘솔창에 마데카솔 이름 등장(텍스트)
-
-        //종료. 대화씬으로 돌아가기
+            EndBlackBox.SetActive(true);
+            StartCoroutine(EndAnimate());
+            serve = true;
+        }
+        else if(serve == true)
+        {
+            //제공하는 씬 호출
+            Debug.Log("serve");
+        }
     }
 
     private IEnumerator EndAnimate()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         Debug.Log("wait");
 
         if (numlily == 6)
         {
             StartCoroutine(LilyEnd());
-            Debug.Log("num==6");
+            Debug.Log("lilynum==6");
         }
         else if (numstock == 6)
         {
             StartCoroutine(StockEnd());
-            Debug.Log("num==6");
+            Debug.Log("stocknum==6");
         }
+
+
     }
 
     private IEnumerator LilyEnd()
     {
+        Making_IMG1.GetComponent<Image>().sprite = lily_making_img;
+        Making_IMG2.GetComponent<Image>().sprite = lily_making_img;
+        Making_IMG3.GetComponent<Image>().sprite = lily_making_img;
+
+        yield return new WaitForSeconds(1f);
+        Making_IMG1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Making_IMG2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Making_IMG3.SetActive(true);
+
         yield return new WaitForSeconds(2f);
+        Making_IMG1.SetActive(false);
+        Making_IMG2.SetActive(false);
+        Making_IMG3.SetActive(false);
+
+        EndBlackBox2.SetActive(true);
+
+        GameObject PlayButton = GameObject.Find("Play_Button");
+        PlayButton.GetComponent<Image>().sprite = serve_img;
+        PlayButton.GetComponent<Button>().interactable = true;
+        Medecine_Name.text = "~마데카솔~";
+        madecassol.SetActive(true);
         Debug.Log("lilyend");
     }
 
