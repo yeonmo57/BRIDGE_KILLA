adcdf3cb8aa381e291d2f7bf3f8abb09b7bf670e
happyjmkk <hepachupa@gmail.com>
2022-06-09 21:22:19 +0900

    day1-1 대화시스템 구현

--- Assets/Scripts/Day1/DialogueSystem.cs
+++ Assets/Scripts/Day1/DialogueSystem.cs
@@ -5,8 +5,101 @@ using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour
 {
+    [SerializeField]
+    private Speaker[] speakers; // 대화에 참여하는 캐릭터들의 UI 배열
+    [SerializeField]
+    private DialogueData[] dialogues; // 현재 분기의 대사 목록 배열
+    [SerializeField] 
+    private bool isAutoStart = true; // 자동 시작 여부
+    private bool isFirst = true; // 최초 1회만 호출하기 위한 변수
+    private int currentDialogueIndex = -1; // 현재 대사 순번
+    private int currentSpeakerIndex = 0; // 현재 말을 하는 화자의 speakers 배열 순번 
 
-    
+    private void Awake()
+    {
+        Setup();
+    }
+
+    private void Setup()
+    {
+        // 모든 대화 관련 게임 오브젝트 비활성화 
+        for(int i =0; i < speakers.Length; ++i)
+        {
+            SetActiveObjects(speakers[i], false);
+            // 캐릭터 이미지는 보이도록 설정
+            speakers[i].spriteRenderer.gameObject.SetActive(true);
+        }
+    }
+
+    public bool UpdateDialogue()
+    {
+        // 대사 분기가 시작될 때 1회만 호출
+        if (isFirst)
+        {
+            // 초기화, 캐릭터 이미지는 활성화, 대사 관련 UI 모두 비활성화
+            Setup();
+            if (isAutoStart) SetNextDialogue();
+
+            isFirst = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 대사가 남아있을 경우 다음 대사 진행
+            if(dialogues.Length > currentDialogueIndex + 1)
+            {
+                SetNextDialogue();
+            }
+            else
+            {
+                for(int i =0; i<speakers.Length; ++i)
+                {
+                    SetActiveObjects(speakers[i], false);
+                    speakers[i].spriteRenderer.gameObject.SetActive(false);
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void SetNextDialogue()
+    {
+        // 이전 화자의 대화 관련 오브젝트 비활성화
+        SetActiveObjects(speakers[currentSpeakerIndex], false);
+
+        // 다음 대사를 진행
+        currentDialogueIndex++;
+
+        // 현재 화자 순번 설정
+        currentSpeakerIndex = dialogues[currentDialogueIndex].speakerIndex;
+
+        // 현재 화자의 대화 관련 오브젝트 활성화
+        SetActiveObjects(speakers[currentSpeakerIndex], true);
+
+        // 현재 화자 이름 텍스트 설정
+        speakers[currentSpeakerIndex].textName.text = dialogues[currentDialogueIndex].name;
+
+        // 현재 화자의 대사 텍스트 설정
+        speakers[currentSpeakerIndex].textDialogue.text = dialogues[currentDialogueIndex].dialogue;
+    }
+
+    private void SetActiveObjects(Speaker speaker, bool visible)
+    {
+        speaker.imageBox.gameObject.SetActive(visible);
+        speaker.textName.gameObject.SetActive(visible);
+        speaker.textDialogue.gameObject.SetActive(visible);
+
+        // 화살표는 대사가 종료되었을 때만 활성화하기 때문에 항상 false
+        speaker.objectArrow.gameObject.SetActive(false);
+
+        // 캐릭터 알파값 변경
+        Color color = speaker.spriteRenderer.color;
+        color.a = visible == true ? 1 : 0.2f;
+        speaker.spriteRenderer.color = color;
+    }
 }
 
 [System.Serializable]
